bf38834b92b7d2410f3189e0a3433d15a3992279
Harisudhan <waytosudhan@gmail.com>
2025-11-17 14:22:21 +0530

    App settings

---
+++ TinyTickets.Api/Services/SasTokenService.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+
+namespace TinyTickets.Api.Services
+{
+    public class SasTokenService
+    {
+        private readonly string _connectionString;
+
+        public SasTokenService(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("StorageAccount")
+                ?? throw new Exception("Storage connection string missing");
+        }
+
+        public string GenerateUploadSas(string container, string fileName)
+        {
+            var containerClient = new BlobContainerClient(_connectionString, container);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            var sas = blobClient.GenerateSasUri(
+                BlobSasPermissions.Write | BlobSasPermissions.Create,
+                DateTime.UtcNow.AddMinutes(20)
+            );
+
+            return sas.ToString();
+        }
+
+        public string GenerateReadSas(string container, string fileName)
+        {
+            var containerClient = new BlobContainerClient(_connectionString, container);
+            var blobClient = containerClient.GetBlobClient(fileName);
+
+            var sas = blobClient.GenerateSasUri(
+                BlobSasPermissions.Read,
+                DateTime.UtcNow.AddMinutes(20)
+            );
+
+            return sas.ToString();
+        }
+    }
+}
