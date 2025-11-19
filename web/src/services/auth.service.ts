fddf9ffb9a381fc9fb9da8f5b9c4037d13a65cea
Harisudhan <waytosudhan@gmail.com>
2025-11-20 01:39:56 +0530

    Fix Auth

--- web/src/services/auth.service.ts
+++ web/src/services/auth.service.ts
@@ -4,13 +4,8 @@ import { msalInstance } from '../config/msal.config';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   login() {
-    msalInstance.loginRedirect({
-      scopes: [
-        'openid',
-        'profile',
-        'email',
-        'api://f2cea967-6192-44ae-aedc-1e6b6a994e5e/access_as_user',
-      ],
+    return msalInstance.loginRedirect({
+      scopes: ['api://f2cea967-6192-44ae-aedc-1e6b6a994e5e/access_as_user'],
     });
   }
 
@@ -22,8 +17,11 @@ export class AuthService {
     return msalInstance.getActiveAccount();
   }
 
-  async getToken(scopes: string[]) {
-    const acc = this.getAccount()!;
-    return msalInstance.acquireTokenSilent({ account: acc, scopes });
+  async getToken() {
+    const account = msalInstance.getActiveAccount()!;
+    return msalInstance.acquireTokenSilent({
+      account,
+      scopes: ['api://f2cea967-6192-44ae-aedc-1e6b6a994e5e/access_as_user'],
+    });
   }
 }
